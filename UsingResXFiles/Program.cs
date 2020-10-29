3d654baaa47fcb9c8864fe893c8fc1fae379e641
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-10-29 13:58:54 +0100

    Dodaj pliki projektów.

---
+++ UsingResXFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using static System.Console;
+
+namespace UsingResXFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resourceManager = new ResourceManager("Resource1", Assembly.GetExecutingAssembly());
+
+            int read = Convert.ToInt32(UsingResXFiles.Resource1.String3);
+
+            //string read = (UsingResXFiles.Resource1.String3);
+
+            WriteLine(read);
+
+            ReadLine();
+        }
+    }
+}
